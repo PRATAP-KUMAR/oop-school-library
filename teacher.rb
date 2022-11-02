0aa785929fef8b6ebac95e573b652a19b5650488
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-02 12:08:32 +0530

    CREATE CONSOLE APP

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Teacher < Person
+  attr_reader	:type
+
   def initialize(age, specialization, name = 'Unknown')
     super(age, name)
     @specialization = specialization
+    @type = 'Teacher'
   end
 
   def can_use_services?
