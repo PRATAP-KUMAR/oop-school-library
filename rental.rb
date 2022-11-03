37aeafa1d7c7aa3234c0f9f70d305e58ad563832
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-03 14:24:47 +0530

    BUG INTRODUCED

--- rental.rb
+++ rental.rb
@@ -8,7 +8,7 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @person = person
-    person.rentals << self
+    person.rentals < self
 
     @book = book
     book.rentals << self
